3b3ee1394e02c705c18566bb5358c9f0d493d3de
Eric Galway <ericwilliamgalway@gmail.com>
2021-09-28 18:34:08 -0400

    Added tower instruction page

---
+++ Assets/Scrips/UI Scrips/InstructionsUI.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionsUI : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas mainCanvas;
+
+    [SerializeField]
+    private Canvas secondCanvas;
+
+    [SerializeField]
+    private Canvas thirdCanvas;
+
+    [SerializeField]
+    private TextMeshProUGUI m_PlayerTowerText;
+    [SerializeField]
+    private TextMeshProUGUI m_PurpleTowerText;
+    [SerializeField]
+    private TextMeshProUGUI m_OrangeTowerText;
+    [SerializeField]
+    private TextMeshProUGUI m_BlueTowerText;
+    [SerializeField]
+    private TextMeshProUGUI m_DrillTowerText;
+
+    [SerializeField]
+    private Image m_goldImage;  
+
+
+    void Start()
+    {
+        mainCanvas.enabled = true;
+        secondCanvas.enabled = false;
+        thirdCanvas.enabled = false;
+
+        DisableAllTowerText();
+    }
+    #region<GameObjective>
+    public void OnPage1NextPressed()
+    {
+        mainCanvas.enabled = false;
+        secondCanvas.enabled = true;
+    }
+    public void OnPage2NextPressed()
+    {
+        DisableAllTowerText();
+        secondCanvas.enabled = false;
+        thirdCanvas.enabled = true;
+    }
+
+    public void OnPage2PreviousPressed()
+    {
+        DisableAllTowerText();
+        secondCanvas.enabled = false;
+        mainCanvas.enabled = true;
+    }
+
+    public void OnPage3PreviousPressed()
+    {
+        thirdCanvas.enabled = false;
+        secondCanvas.enabled = true;
+    }
+
+    public void OnReturnToMainMenuPressed()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    #endregion<GameObjective>
+
+
+    #region<Towers>
+    void DisableAllTowerText()
+    {
+        m_PlayerTowerText.enabled = false;
+        m_PurpleTowerText.enabled = false;
+        m_OrangeTowerText.enabled = false;
+        m_BlueTowerText.enabled = false;
+        m_DrillTowerText.enabled = false;
+        m_goldImage.enabled = false;
+    }
+
+    public void OnPlayerTowerPressed()
+    {
+        m_PlayerTowerText.enabled = !m_PlayerTowerText.enabled;
+    }
+
+    public void OnPurpleTowerPressed()
+    {
+        m_PurpleTowerText.enabled = !m_PurpleTowerText.enabled;
+    }
+
+    public void OnOrangeTowerPressed()
+    {
+        m_OrangeTowerText.enabled = !m_OrangeTowerText.enabled;
+    }
+
+    public void OnBlueTowerPressed()
+    {
+        m_BlueTowerText.enabled = !m_BlueTowerText.enabled;
+    }
+
+    public void OnDrillPressed()
+    {
+        m_DrillTowerText.enabled = !m_DrillTowerText.enabled;
+        m_goldImage.enabled = m_DrillTowerText.enabled;
+    }
+    #endregion<Towers>
+}
