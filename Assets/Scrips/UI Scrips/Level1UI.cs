2ebf8536f936d2721c3fd91bff3cbd72516c3f88
Eric Galway <ericwilliamgalway@gmail.com>
2021-09-28 21:51:09 -0400

    Added pause menu, reworked Level1 UI

--- Assets/Scrips/UI Scrips/Level1UI.cs
+++ Assets/Scrips/UI Scrips/Level1UI.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine;
 
 public class Level1UI : MonoBehaviour
 {
+    [SerializeField]
+    private Canvas pauseCanvas;
     // Start is called before the first frame update
+    private void Start()
+    {
+        pauseCanvas.enabled = false;
+    }
 
     public void OnStartPressed()
     {
         Debug.Log("Start button pressed");
     }
-    public void OnQuitGamePressed()
+    public void PauseMenuPressed()
     {
-        Debug.Log("Quit button pressed");
+        Time.timeScale = 0;
+        pauseCanvas.enabled = true;
+    }
+    public void OnResumePlayPressed()
+    {
+        Time.timeScale = 1;
+        pauseCanvas.enabled = false;
+    }
+    public void OnMainMenuPressed()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void OnQuitPressed()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
     }
 }
